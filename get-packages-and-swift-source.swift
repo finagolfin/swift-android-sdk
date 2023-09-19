243dc21deddcfd68b5c493424fd25881b442454d
Finagolfin <repo@butta.fastem.com>
2023-09-19 15:20:48 +0530

    Fix crypto package tags checked out on CI

--- get-packages-and-swift-source.swift
+++ get-packages-and-swift-source.swift
@@ -13,7 +13,7 @@ let extraSwiftRepos = ["swift-llbuild", "swift-package-manager", "swift-driver",
                        "swift-collections", "swift-certificates", "swift-asn1"]
 let renameRepos = ["swift-llbuild" : "llbuild", "swift-package-manager" : "swiftpm", "Yams" : "yams"]
 var repoTags = ["swift-system" : "1.1.1", "swift-collections" : "1.0.1", "swift-asn1" : "0.8.0",
-                "swift-certificates" : "0.4.1", "Yams" : "5.0.1", "swift-argument-parser" : "1.2.2",
+                "swift-certificates" : "0.6.0", "Yams" : "5.0.1", "swift-argument-parser" : "1.2.2",
                 "swift-crypto" : "2.5.0"]
 if ProcessInfo.processInfo.environment["BUILD_SWIFT_PM"] != nil {
   termuxPackages += ["ncurses", "libsqlite"]
@@ -49,7 +49,8 @@ if tagExtract.numberOfMatches(in: SWIFT_TAG, range: tagRange) == 1 {
 }
 
 if swiftBranch == "RELEASE" {
-  repoTags["swift-certificates"] = "0.7.0"
+  repoTags["swift-asn1"] = "0.7.0"
+  repoTags["swift-certificates"] = "0.4.1"
   sdkDir = "swift-release-android-\(ANDROID_ARCH)-24-sdk"
 } else {
   sdkDir = "swift-\(swiftVersion == "" ? "trunk" : "devel")-android-\(ANDROID_ARCH)-\(swiftSnapshotDate)-24-sdk"
