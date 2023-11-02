7af313f75dd821d4f9e424e4f04601c58186e61d
Finagolfin <repo@butta.fastem.com>
2023-11-02 19:42:06 +0530

    Update 5.10 crypto tags checked out on CI

--- get-packages-and-swift-source.swift
+++ get-packages-and-swift-source.swift
@@ -12,9 +12,9 @@ let extraSwiftRepos = ["swift-llbuild", "swift-package-manager", "swift-driver",
                        "Yams", "indexstore-db", "sourcekit-lsp", "swift-system",
                        "swift-collections", "swift-certificates", "swift-asn1"]
 let renameRepos = ["swift-llbuild" : "llbuild", "swift-package-manager" : "swiftpm", "Yams" : "yams"]
-var repoTags = ["swift-system" : "1.1.1", "swift-collections" : "1.0.1", "swift-asn1" : "0.8.0",
-                "swift-certificates" : "0.6.0", "Yams" : "5.0.1", "swift-argument-parser" : "1.2.3",
-                "swift-crypto" : "2.5.0"]
+var repoTags = ["swift-system" : "1.1.1", "swift-collections" : "1.0.1", "swift-asn1" : "1.0.0",
+                "swift-certificates" : "1.0.1", "Yams" : "5.0.1", "swift-argument-parser" : "1.2.3",
+                "swift-crypto" : "3.0.0"]
 if ProcessInfo.processInfo.environment["BUILD_SWIFT_PM"] != nil {
   termuxPackages += ["ncurses", "libsqlite"]
 }
@@ -52,13 +52,9 @@ if swiftBranch == "RELEASE" {
   repoTags["swift-argument-parser"] = "1.2.2"
   repoTags["swift-asn1"] = "0.7.0"
   repoTags["swift-certificates"] = "0.4.1"
+  repoTags["swift-crypto"] = "2.5.0"
   sdkDir = "swift-release-android-\(ANDROID_ARCH)-24-sdk"
 } else {
-  if swiftVersion == "" {
-    repoTags["swift-asn1"] = "1.0.0"
-    repoTags["swift-certificates"] = "1.0.1"
-    repoTags["swift-crypto"] = "3.0.0"
-  }
   sdkDir = "swift-\(swiftVersion == "" ? "trunk" : "devel")-android-\(ANDROID_ARCH)-\(swiftSnapshotDate)-24-sdk"
 }
 
