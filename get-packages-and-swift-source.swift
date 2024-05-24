d6b9865db113a7d7d240d7b9f4292ceba4c98664
Finagolfin <finagolfin@tuta.io>
2024-05-24 12:40:21 +0530

    Update Yams tag checked out on CI

--- get-packages-and-swift-source.swift
+++ get-packages-and-swift-source.swift
@@ -13,7 +13,7 @@ let extraSwiftRepos = ["swift-llbuild", "swift-package-manager", "swift-driver",
                        "swift-collections", "swift-certificates", "swift-asn1"]
 let renameRepos = ["swift-llbuild" : "llbuild", "swift-package-manager" : "swiftpm", "Yams" : "yams"]
 var repoTags = ["swift-system" : "1.2.1", "swift-collections" : "1.0.5", "swift-asn1" : "1.0.0",
-                "swift-certificates" : "1.0.1", "Yams" : "5.0.1", "swift-argument-parser" : "1.2.3",
+                "swift-certificates" : "1.0.1", "Yams" : "5.0.6", "swift-argument-parser" : "1.2.3",
                 "swift-crypto" : "3.0.0"]
 if ProcessInfo.processInfo.environment["BUILD_SWIFT_PM"] != nil {
   termuxPackages += ["ncurses", "libsqlite"]
@@ -50,6 +50,7 @@ if tagExtract.numberOfMatches(in: SWIFT_TAG, range: tagRange) == 1 {
 
 if swiftBranch == "RELEASE" {
   repoTags["swift-system"] = "1.1.1"
+  repoTags["Yams"] = "5.0.1"
   sdkDir = "swift-release-android-\(ANDROID_ARCH)-24-sdk"
 } else {
   sdkDir = "swift-\(swiftVersion == "" ? "trunk" : "devel")-android-\(ANDROID_ARCH)-\(swiftSnapshotDate)-24-sdk"
