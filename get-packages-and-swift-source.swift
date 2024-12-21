bb1cef608cc8c327705f70253479c1a0ccffe803
Finagolfin <finagolfin@tuta.io>
2024-12-21 15:57:46 +0530

    Add a dependency on the CI for trunk sourcekit-lsp, swift-lmdb

--- get-packages-and-swift-source.swift
+++ get-packages-and-swift-source.swift
@@ -12,7 +12,7 @@ let swiftRepos = ["llvm-project", "swift", "swift-experimental-string-processing
 
 let extraSwiftRepos = ["swift-llbuild", "swift-package-manager", "swift-driver",
                        "swift-tools-support-core", "swift-argument-parser", "swift-crypto",
-                       "Yams", "indexstore-db", "sourcekit-lsp", "swift-system",
+                       "Yams", "indexstore-db", "sourcekit-lsp", "swift-system", "swift-lmdb",
                        "swift-certificates", "swift-asn1", "swift-toolchain-sqlite"]
 let appleRepos = ["swift-argument-parser", "swift-crypto", "swift-system", "swift-collections", "swift-certificates", "swift-asn1"]
 let renameRepos = ["swift-llbuild" : "llbuild", "swift-package-manager" : "swiftpm", "Yams" : "yams"]
